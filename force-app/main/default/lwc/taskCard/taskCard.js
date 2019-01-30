cd94432567ecce2928f433bf35ac8006923e5eef
Sean Cuevo <seanpat09@gmail.com>
2019-01-30 16:26:31 -0700

    apex method call example, formatting and adding some hints

--- force-app/main/default/lwc/taskCard/taskCard.js
+++ force-app/main/default/lwc/taskCard/taskCard.js
@@ -1,5 +1,23 @@
 import { LightningElement, api } from 'lwc';
+import saveTasks from '@salesforce/apex/TaskBoardController.saveTasks';
 
 export default class TaskCard extends LightningElement {
+    //NOTE: @api properties are immutable
     @api task
+
+    edit() {
+        console.log('enable editing here!');
+    }
+
+    //Basic wiring to a save method in Salesforce server
+    save() {
+        let taskToSave = Object.assign({}, this.task);
+        saveTasks({ tasks: [taskToSave] })
+            .then(() => {
+                console.log('Success!');
+            })
+            .catch(() => {
+                console.log('Something went wrong...');
+            })
+    }
 }
