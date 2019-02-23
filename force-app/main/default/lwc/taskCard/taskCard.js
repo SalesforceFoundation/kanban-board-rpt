c8fc8a64101955ccc27a69a8a75a1dd32eca750d
Mansoor Siddeeq <mansoor@msiddeeq.com>
2019-02-23 00:58:12 -0500

    completed criteria 1-3

--- force-app/main/default/lwc/taskCard/taskCard.js
+++ force-app/main/default/lwc/taskCard/taskCard.js
@@ -1,20 +1,37 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import saveTasks from '@salesforce/apex/TaskBoardController.saveTasks';
+import DESCRIPTION from '@salesforce/schema/Task.Description';
 
 export default class TaskCard extends LightningElement {
     //NOTE: @api properties are immutable
-    @api task
+    @api task;
+    @track editing; // track editing status of the component
+    updatedComment; // Store new comment; no need to track since its not reactive
 
-    edit() {
-        console.log('enable editing here!');
+    handleEdit() {
+        this.editing = true;
+    }
+
+    handleCancel() {
+        // close the textarea field without saving
+        this.editing = false;
+    }
+
+    handleDescChange(event) {
+        if (this.editing) {
+            this.updatedComment = event.detail.value;
+            // console.log(this.updatedComment);
+        }
     }
 
     //Basic wiring to a save method in Salesforce server
-    save() {
-        let taskToSave = Object.assign({}, this.task);
+    handleSave() {
+        let taskToSave = Object.assign({}, this.task, {Description: this.updatedComment});
         saveTasks({ tasks: [taskToSave] })
             .then(() => {
-                console.log('Success!');
+                console.log('Success!', JSON.stringify(taskToSave.Description));
+                console.log('New task: ' + JSON.stringify(this.task.Description));
+                this.editing = false;
             })
             .catch(() => {
                 console.log('Something went wrong...');
